cf8a76deea6f72e5c74a86c13e9153e751d8fcd0
Quan Nguyen <huu2uan@gmail.com>
2017-10-13 22:06:32 +0700

    initial commit

---
+++ fortune.clj
@@ -0,0 +1,27 @@
+(require '[clj-http.client :as http]
+         '[clojure.java.shell :refer [sh]]
+         '[clojure.string :as str])
+
+;; twitter
+(def tweets (into #{} (str/split (slurp "tweets.txt") #"\n%\n")))
+
+(defn twit [msg]
+  (sh "bash" "-c" (str "twurl -d 'status=" msg "' /1.1/statuses/update.json")))
+
+;; github
+(def quotes (->> (http/get "https://api.github.com/repos/icy/fortune-vn/contents/data"
+                           {:headers {"User-Agent" "fortune-vn"}
+                            :as      :json})
+                 :body
+                 (map (comp slurp :download_url))
+                 (mapcat #(str/split % #"\n%\n"))
+                 (map str/trim)))
+
+;; main
+(let [quote (->> quotes
+                 (filter #(<= (count %) 140))
+                 (remove #(contains? tweets %))
+                 rand-nth)]
+  (println "twitting quote:\n" quote)
+  (twit quote)
+  (spit "tweets.txt" (str quote "\n%\n") :append true))
